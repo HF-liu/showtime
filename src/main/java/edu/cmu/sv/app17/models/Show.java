eed7b5f1be5ebbfe6d4f4fdfea3dff44365af031
Hefei Liu <liuhf24@gmail.com>
2017-11-06 19:25:03 -0800

    Change Show Class

--- src/main/java/edu/cmu/sv/app17/models/Show.java
+++ src/main/java/edu/cmu/sv/app17/models/Show.java
@@ -4,33 +4,15 @@ package edu.cmu.sv.app17.models;
 public class Show {
      String showId = null;
      String showName;
-     Number seasons;
-     String channel;
+     String channelId;
      String intro;
-     String cast;
-     Double showRating;
-     String showPhoto;
-     String showTime;
-     String nextEpisode;
      String showCategory;
-     String reviews;
-     Number showFollow;
 
-    public Show(String showName, Number seasons, String channel, String intro,
-                String cast, Double showRating, String showPhoto, String showTime,
-                String nextEpisode, String showCategory, String reviews, Number showFollow) {
+    public Show(String showName, String channelId, String intro, String showCategory,) {
         this.showName = showName;
-        this.seasons = seasons;
-        this.channel = channel;
+        this.channelId = channelId;
         this.intro = intro;
-        this.cast = cast;
-        this.showRating = showRating;
-        this.showPhoto = showPhoto;
-        this.showTime = showTime;
-        this.nextEpisode = nextEpisode;
         this.showCategory = showCategory;
-        this.reviews = reviews;
-        this.showFollow = showFollow;
     }
 
     public void setId(String id) {
