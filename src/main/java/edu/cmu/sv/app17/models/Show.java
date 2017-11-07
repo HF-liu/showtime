98fae850a6e7c722f5a81cee444f7f2f698a364a
Hubert <jinjiangbei@163.com>
2017-11-06 20:59:31 -0800

    Update Show.java

--- src/main/java/edu/cmu/sv/app17/models/Show.java
+++ src/main/java/edu/cmu/sv/app17/models/Show.java
@@ -1,18 +1,21 @@
 package edu.cmu.sv.app17.models;
 
-
 public class Show {
      String showId = null;
      String showName;
      String channelId;
      String intro;
      String showCategory;
+     String showphoto;
+     Integer showRating;
 
-    public Show(String showName, String channelId, String intro, String showCategory) {
+    public Show(String showName, String channelId, String intro, String showCategory, String showphoto, Integer showRating) {
         this.showName = showName;
         this.channelId = channelId;
         this.intro = intro;
         this.showCategory = showCategory;
+        this.showphoto = showphoto;
+        this.showRating = showRating;
     }
 
     public void setId(String id) {
