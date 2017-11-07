75035cc863061e23dfe1c7b163a58e57b375fd12
Hefei Liu <liuhf24@gmail.com>
2017-11-06 19:11:37 -0800

    Change User Class

--- src/main/java/edu/cmu/sv/app17/models/User.java
+++ src/main/java/edu/cmu/sv/app17/models/User.java
@@ -4,26 +4,10 @@ public class User {
     String userId = null;
     String userName;
     String email;
-    String phone;
-    String profilePhoto;
-    String favs;
-    Number showNum;
-    String reviews;
-    String friends;
-    String joinDate;
 
-    public User(String userName, String email, String phone,
-                String profilePhoto, String favs, Number showNum,
-                String reviews, String friends, String joinDate) {
+    public User(String userName, String email) {
         this.userName = userName;
         this.email = email;
-        this.phone = phone;
-        this.profilePhoto = profilePhoto;
-        this.favs = favs;
-        this.showNum = showNum;
-        this.reviews = reviews;
-        this.friends = friends;
-        this.joinDate = joinDate;
     }
     public void setId(String id) {
         this.userId = id;
