1b712583e051f6fa62f5f1fb447063043d4ba8ef
Hefei Liu <liuhf24@gmail.com>
2017-11-06 19:23:09 -0800

    Change Review Class

--- src/main/java/edu/cmu/sv/app17/models/Review.java
+++ src/main/java/edu/cmu/sv/app17/models/Review.java
@@ -1,29 +1,22 @@
 package edu.cmu.sv.app17.models;
 
+import java.util.Date;
+
 public class Review {
     String reviewId = null;
     String showId;
-    String episodeId;
     String userId;
-    int rate;
-    String createDate;
-    String editDate;
+    Date createDate;
     String reviewTopic;
     String reviewContent;
-    int likes;
 
-    public Review(String showId, String episodeId, String userId,
-                  int rate, String createDate, String editDate,
-                  String reviewTopic, String reviewContent, int likes) {
+    public Review(String showId,String userId,
+                  Date createDate,String reviewTopic, String reviewContent) {
         this.showId = showId;
-        this.episodeId = episodeId;
         this.userId = userId;
-        this.rate = rate;
         this.createDate = createDate;
-        this.editDate = editDate;
         this.reviewTopic = reviewTopic;
         this.reviewContent = reviewContent;
-        this.likes = likes;
     }
 
     public void setId(String id) {
