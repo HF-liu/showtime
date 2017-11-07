8c3845dccb62cd76bf998bc8b7cb1443e31e3441
Hefei Liu <liuhf24@gmail.com>
2017-11-06 19:30:58 -0800

    Change Fav Class

--- src/main/java/edu/cmu/sv/app17/models/Fav.java
+++ src/main/java/edu/cmu/sv/app17/models/Fav.java
@@ -4,18 +4,10 @@ public class Fav {
     String favId = null;
     String userId;
     String showId;
-    String favShows;
-    String showPics;
-    String latestWatched;
-    String castNews;
 
     public Fav(String userId, String showId, String favShows, String showPics, String latestWatched, String castNews) {
         this.userId = userId;
         this.showId = showId;
-        this.favShows = favShows;
-        this.showPics = showPics;
-        this.latestWatched = latestWatched;
-        this.castNews = castNews;
     }
 
     public void setId(String id) {
