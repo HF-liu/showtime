c14705132aab49001385d66efa6bc36e085592be
Hefei Liu <liuhf24@gmail.com>
2017-11-06 19:29:49 -0800

    Change Channel Class

--- src/main/java/edu/cmu/sv/app17/models/Channel.java
+++ src/main/java/edu/cmu/sv/app17/models/Channel.java
@@ -4,14 +4,10 @@ public class Channel {
     String channelId = null;
     String channelName;
     String channelLogo;
-    String showList;
-    Number showNumber;
 
     public Channel(String channelName, String channelLogo, String showList, Number showNumber) {
         this.channelName = channelName;
         this.channelLogo = channelLogo;
-        this.showList = showList;
-        this.showNumber = showNumber;
     }
 
 
