5a37425a354c4404c56d5d1684d57166476d54bc
Hefei Liu <liuhf24@gmail.com>
2017-11-11 14:44:35 -0800

    Update NewsInterface

--- src/main/java/edu/cmu/sv/app17/rest/NewsInterface.java
+++ src/main/java/edu/cmu/sv/app17/rest/NewsInterface.java
@@ -166,5 +166,25 @@ public class NewsInterface {
         }
     }
 
+    @DELETE
+    @Path("{id}")
+    @Produces({ MediaType.APPLICATION_JSON})
+    public APPResponse delete(@Context HttpHeaders headers,@PathParam("id") String id) {
+        BasicDBObject query = new BasicDBObject();
+        query.put("_id", new ObjectId(id));
+        try {
+            Authorization.checkAdmin(headers);
+            DeleteResult deleteResult = newsCollection.deleteOne(query);
+            if (deleteResult.getDeletedCount() < 1)
+                throw new APPNotFoundException(66, "Could not delete");
+        } catch(APPUnauthorizedException e){
+            throw new APPUnauthorizedException(70,"Not authorized.");
+        }  catch(Exception e) {
+            throw new APPInternalServerException(99,"Something happens!");
+        }
+
+        return new APPResponse();
+    }
+
 
 }
