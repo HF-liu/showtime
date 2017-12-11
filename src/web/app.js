59c979849cc950d5b3d9b54b7aaae20a17c2182e
Hefei Liu <liuhf24@gmail.com>
2017-12-10 17:06:59 -0800

    Update login page

--- src/web/app.js
+++ src/web/app.js
@@ -19,9 +19,8 @@ $(function() {
             dataType: "json",
             contentType: "application/json; charset=utf-8"
         }).done(function(data){
-            $("#greeting").text("Hello " + data.content.firstName);
+            // $("#greeting").text("Hello " + data.content.firstName);
             $("#loadcontent").show();
-            $("#resourceTable").find(".cloned").remove();
             token = data.content.token;
             userId = data.content.userId;
             localStorage.setItem("token", token);
@@ -44,8 +43,7 @@ $(function() {
                     })
             location.href = "show/show.html"
         }).fail(function(data){
-                $("#greeting").text("You might want to try it again");
-                $("#loadcontent").hide();
+                alert("Wrong userid or password. Please try again.");
             })
     })
 //
@@ -102,6 +100,80 @@ $(function() {
     //
     // }
 
+    $('#newUser').on('show.bs.modal', function (event) {
+        // var button = $(event.relatedTarget) // Button that triggered the modal
+
+        // var reviewId = button.parents("tr").attr("id");
+        // var topic = button.parents("tr").find("#Topic").text();
+        // var content = button.parents("tr").find("#Content").text();
+        // If necessary, you could initiate an AJAX request here (and then do the updating in a callback).
+        // Update the modal's content. We'll use jQuery here, but you could use a data binding library or other methods instead.
+        // var modal = $(this)
+        // modal.find('.modal-title').text('Now you can edit this review');
+        // // modal.find('.modal-body input').val(topic);
+        // $('#topictext').val(topic);
+        // $('#contenttext').val(content);
+        // var editedtopic = modal.find('.modal-body input').text();
+        // var editedcontent = modal.find('.modal-body textarea').text();
+        // $("#registername").val("Input you name");
+
+        $("#postnewuser").click(function (e) {
+            var username = $("#registername").val();
+            var email = $("#registeremail").val();
+            var pwd = $("#registerpwd").val();
+            var rptpwd = $("#repeatpwd").val();
+            if(username == "" || email == "" || pwd == "" || rptpwd == ""){
+                alert("Please fill all blanks.");
+            } else if((pwd !== rptpwd)){
+                alert("You should re-enter the same password you want to set.");
+            } else {
+                var obj = Object();
+                obj.userName = $('#registername').val();
+                obj.email = $('#registeremail').val();
+                obj.password = $('#registerpwd').val();
+                var patchinfo = JSON.stringify(obj);
+                jQuery.ajax({
+                    url: "/api/users",
+                    type: "POST",
+                    data: patchinfo,
+                    dataType: "json",
+                    contentType: "application/json; charset=utf-8",
+                    // beforeSend: function (xhr) {
+                    //     xhr.setRequestHeader("Authorization", token);
+                    // }
+                })
+                    .done(function (data) {
+                        alert("Welcome to Showtime!");
+                        $("#closenew").trigger("click");
+                        $("#inputEmail").val(data.content.email);
+                        $("#inputPassword").val(data.content.password);
+                    })
+                    .fail(function (data) {
+                        alert("Failed to register!");
+                    })
+            }
+            // jQuery.ajax ({
+            //
+            //     url:  "/api/users",
+            //     type: "POST",
+            //     data: JSON.stringify({reviewTopic:$('#topictext').val(), reviewContent: $('#contenttext').val()}),
+            //     dataType: "json",
+            //     contentType: "application/json; charset=utf-8",
+            //     beforeSend: function (xhr) {
+            //         xhr.setRequestHeader ("Authorization", token);
+            //     }
+            // })
+            //     .done(function(data){
+            //         $("#closethis").trigger("click");
+            //     })
+            //     .fail(function(data){
+            //         alert("Failed to edit!");
+            //     })
+        });
+    });
+
 })
+
+
 
 
